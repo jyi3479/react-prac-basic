ceb416737970381a6696b6c8a4f9ffb19a44584e
jyi3479 <94282246+jyi3479@users.noreply.github.com>
2022-02-01 20:47:06 +0900

    HW2

---
+++ quiz_hw/src/start.js
@@ -0,0 +1,62 @@
+import React from "react";
+import styled from "styled-components";
+import img from "./라이언.jpg";
+
+const Start = (props) => {
+  console.log(props);
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        alignItems: "center", // 1. flex의 정렬을 담당하는 두 속성
+        justifyContent: "center", // 2. flex의 정렬을 담당하는 두 속성
+        flexDirection: "column", //flex에서 기본값이 row로 정렬하기 때문에 column으로 바꿔줌
+        padding: "16px",
+        boxSizing: "border-box", //테두리를 포함한 넓이가 이 요소의 넓이라고 지정
+      }}
+    >
+      <img
+        src={img}
+        style={{
+          width: "60vw",
+          margin: "16px",
+        }}
+      />
+      <h1 style={{ fontSize: "1.5em", lineHeight: "1.5", textAlign: "center" }}>
+        나는
+        <span
+          style={{
+            backgroundColor: "#caecb2",
+            padding: "5px 10px",
+            borderRadius: "30px",
+          }}
+        >
+          {props.name}
+        </span>
+        에 대해 얼마나 알고 있을까?
+      </h1>
+      <input
+        style={{
+          border: "1px solid orange",
+          borderRadius: "30px",
+          padding: "10px",
+          width: "100%",
+        }}
+      />
+      <button
+        style={{
+          padding: "10px 36px",
+          backgroundColor: "#ffcc6d",
+          border: "#ffcc6d",
+          borderRadius: "30px",
+          margin: "40px",
+        }}
+      >
+        시작하기
+      </button>
+    </div>
+  );
+};
+
+export default Start;

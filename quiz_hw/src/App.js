6dde9817799282a4692e866623df7d6937eb9d1f
jyi3479 <94282246+jyi3479@users.noreply.github.com>
2022-02-01 20:47:06 +0900

    2주차 HW

--- quiz_hw/src/App.js
+++ quiz_hw/src/App.js
@@ -1,60 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import Start from "./Start";
 
 function App() {
+  const [name, setName] = React.useState("라이언");
   return (
-    <AppWrap>
-      <Container>
-        <img src="img/moomin.png" />
-        <Title>
-          나는 <span>무민</span>에 대해 얼마나 알고 있을까?
-        </Title>
-        <InputBox>
-          <input />
-        </InputBox>
-        <Button>시작하기</Button>
-      </Container>
-    </AppWrap>
+    <div
+      className=""
+      style={{
+        maxWidth: "350px",
+        margin: "auto",
+      }}
+    >
+      <Start name={name} />
+    </div>
   );
 }
-
-const AppWrap = styled.div`
-  display: flex;
-  background-color: lavender;
-  height: 100vh;
-  width: 100vw;
-  flex-direction: column;
-`;
-
-const Container = styled.div`
-  background-color: #fff;
-  width: 50vw;
-  max-width: 350px;
-  margin: auto;
-  height: 80vh;
-  padding: 16px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-
-  img {
-  }
-`;
-
-const Title = styled.div`
-  color: slateblue;
-  text-align: center;
-`;
-
-const InputBox = styled.div`
-  background-color: #fff;
-  width: 50vw;
-  max-width: 350px;
-  margin: auto;
-  padding: 16px;
-  border: 1px solid #ddd;
-  border-radius: 5px;
-`;
-
-const Button = styled.button``;
 
 export default App;
